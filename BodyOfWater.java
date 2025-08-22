2fb3786d349998a3bf827472d0e597960746390d
Karena <0716karena@gmail.com>
2025-08-22 13:46:43 -0700

    hi

--- BodyOfWater.java
+++ BodyOfWater.java
@@ -1,1 +1,27 @@
+public class BodyOfWater{
+    private String name;
+    private int largestDiameter;
+    private int avgDepth;
+    private boolean isSaltWater;
+    public BodyOfWater(String n, int l, int d, boolean s){
+
+        name=n;
+        largestDiameter=l;
+        avgDepth=d;
+        isSaltWater=s;
+
+    }
+    public BodyOfWater(String n, int d){
+        name=n;
+        avgDepth=d;
+    }
+    public void printState(){
+        System.out.println("Name: " + name);
+        System.out.println("largest diameter: " + largestDiameter);
+    }
+}
+    //attributes: name, largestDiameter, avgDepth, isSaltWater
+
+
+
 
